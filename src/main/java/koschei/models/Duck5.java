4ffcc056c0d35b7a88bd4b2a55d5dd53a1937714
Anton Shevchuk <AntonShevchuk94@gmail.com>
2026-09-05 19:20:20 +0300

    Spring types of wiring task

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,16 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Duck5 {
+public class Duck5 { // DI связываем через поля и анотацию
+    @Autowired
+    private Egg6 egg;
+
 
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg.toString();
     }
 }
